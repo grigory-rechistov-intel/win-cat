a146730673b260e5d4870d75ef4f3b38fbbbff92
Grigory Rechistov <grigory.rechistov@intel.com>
2023-12-27 10:52:42 +0100

    Add dynamic sensing for coverage binary
    It may be named differently in different installations.

--- characterization/collect-coverage.py
+++ characterization/collect-coverage.py
@@ -3,6 +3,15 @@
 import sys
 from test import main as real_main
 
+def detect_coverage_command():
+    from shutil import which
+    for candidate in ("python3-coverage", "coverage3"):
+        if which(candidate):
+            return candidate
+    raise Exception("No coverage binary found")
+
+
 if __name__ == "__main__":
-    coverage_cmd = ["python3-coverage", "run", "-a"]
-    sys.exit(real_main(coverage_cmd))
+    coverage = detect_coverage_command()
+    coverage_cmdline = [coverage, "run", "-a"]
+    sys.exit(real_main(coverage_cmdline))
